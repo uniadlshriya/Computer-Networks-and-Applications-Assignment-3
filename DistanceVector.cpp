a8ff9c94d9d148a249b4db1ba0d0723a656373ef
Shriya Somasundara Narasimha <a1741690@adelaide.edu.au>
2025-05-27 22:13:30 +0930

    printing only Hello World to debug Autograder issue removed couple of include libraries

--- DistanceVector.cpp
+++ DistanceVector.cpp
@@ -1,22 +1,12 @@
-#include <iostream>
-#include <fstream>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <limits>
+/* code for Distance vector*/
+
 #include <string>
 #include <sstream>
 #include <cstdlib>
-#include <iomanip>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
-
-const int INF = numeric_limits<int>::max();
-
-unordered_map<char, unordered_map<char, int>> costedge;
-unordered_map<char, unordered_map<char, int>> distedge;
-unordered_map<char, unordered_map<char, char>> nextedge;
-set<char> router;
 
 
 /* Main to read the file from command line*/
